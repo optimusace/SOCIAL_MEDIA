8c684649dfaeb33ecc88ab1af60da13eabea0e30
Prabin Bastakoti <Prabinbastakoti1@gmail.com>
2024-01-17 16:14:18 +0545

    build comment api

---
+++ controllers/userCommentController.js
@@ -0,0 +1,142 @@
+const CommentService = require('../services/userCommentService');
+
+class CommentController {
+  // get all comments of a single user
+  static getallCommentsOfUser = async (req, res) => {
+    try {
+      const userId = req.params.userId;
+      const comments = await CommentService.getallCommentsOfUser(userId);
+      if (!comments) {
+        return res
+          .status(404)
+          .json({ success: false, message: 'Sorry!! Unable to find comments' });
+      }
+      res
+        .status(200)
+        .json({ success: true, message: 'comments available', data: comments });
+    } catch (err) {
+      res.status(500).json({
+        success: false,
+        message: 'Internal Server Error',
+        error: err.message,
+      });
+    }
+  };
+
+  // get all comments from a single post
+  static getSpecificComments = async (req, res) => {
+    try {
+      const postId = req.params.postId;
+      const comments = await CommentService.getSpecificComments(postId);
+
+      if (!comments) {
+        return res
+          .status(404)
+          .json({ success: false, message: 'Sorry!! Unable to find comments' });
+      }
+      res
+        .status(200)
+        .json({ success: true, message: 'comments available', data: comments });
+    } catch (err) {
+      res.status(500).json({
+        success: false,
+        message: 'Internal Server Error',
+        error: err.message,
+      });
+    }
+  };
+
+  // Create new comment
+  static createComment = async (req, res) => {
+    try {
+      const postId = req.params.postId;
+      const userId = req.params.userId;
+      const data = req.body;
+
+      const createdComment = await CommentService.createComment(
+        postId,
+        userId,
+        data
+      );
+
+      if (!createdComment) {
+        return res
+          .status(404)
+          .json({ success: false, message: 'Unable to create new comment' });
+      }
+
+      res.status(200).json({
+        success: true,
+        message: 'Successfully created comment',
+        data: createdComment,
+      });
+    } catch (err) {
+      res.status(500).json({
+        success: false,
+        message: 'Internal Server Error',
+        error: err.message,
+      });
+    }
+  };
+
+  //Update the comment
+  static updateComment = async (req, res) => {
+    try {
+      const id = req.params.id;
+      const userId = req.params.userId;
+      const data = req.body;
+
+      const updatedComment = await CommentService.updateComment(
+        id,
+        userId,
+        data
+      );
+
+      if (!updatedComment) {
+        return res
+          .status(404)
+          .json({ success: false, message: 'Unable to update the comment' });
+      }
+
+      res.status(200).json({
+        success: true,
+        message: 'Successfully updated the comment',
+        data: createdComment,
+      });
+    } catch (err) {
+      res.status(500).json({
+        success: false,
+        message: 'Internal Server Error',
+        error: err.message,
+      });
+    }
+  };
+
+  //Delete the comment
+  static deleteComment = async (req, res) => {
+    try {
+      const id = req.params.id;
+      const userId = req.params.userId;
+
+      const deletedComment = await CommentService.deleteComment(id, userId);
+      if (!deletedComment) {
+        return res
+          .status(404)
+          .json({ success: false, message: 'Cannot delete comment' });
+      }
+      res.status(200).json({
+        success: true,
+        message: 'Successfully deleted comment',
+        data: deletedComment,
+      });
+    } catch (err) {
+      res.status(500).json({
+        success: false,
+        message: 'Internal Server Error',
+        error: err.message,
+      });
+    }
+  };
+}
+
+module.exports = CommentController;
--- services/userCommentService.js
+++ services/userCommentService.js
@@ -4,11 +4,53 @@ const UserComment = require('../models/usersCommentModel');
 const mongoose = require('mongoose');
 
 class CommentService {
-  static createComment = async (postId, userId, data) => {
+  // get all comments of a single user
+  static getallCommentsOfUser = async (userid) => {
     try {
-      if (!mongoose.Types.ObjectId.isValid(userId)) {
+      if (!mongoose.Types.ObjectId.isValid(userid)) return null;
+
+      const user = await UserBasicInfo.find(userid);
+      if (!user) return null;
+
+      const comments = await UserComment.find({ userId: userId });
+
+      if (comments.length > 0) {
+        return comments;
+      } else {
         return null;
       }
+    } catch (err) {
+      throw err;
+    }
+  };
+
+  // get all comments from a single post
+  static getSpecificComments = async (postId) => {
+    try {
+      if (!mongoose.Types.ObjectId.isValid(postId)) return null;
+
+      const post = await UserPost.findById(postId);
+      if (!post) return null;
+
+      const comments = await UserComment.find({ postId: postId });
+
+      if (comments.length > 0) {
+        return comments;
+      } else {
+        return null;
+      }
+    } catch (err) {
+      throw err;
+    }
+  };
+
+  // Create new comment
+  static createComment = async (postId, userId, data) => {
+    try {
+      if (!mongoose.Types.ObjectId.isValid(userId)) return null;
+
+      if (!mongoose.Types.ObjectId.isValid(postId)) return null;
+
       const user = await UserBasicInfo.findById(userId);
       const post = await UserPost.findById(postId);
 
@@ -24,6 +66,61 @@ class CommentService {
         postId,
         comment: data,
       });
+
+      return newComment;
+    } catch (err) {
+      throw err;
+    }
+  };
+
+  //Update the comment
+  static updateComment = async (id, userId, data) => {
+    try {
+      if (!mongoose.Types.ObjectId.isValid(id)) return null;
+
+      if (!mongoose.Types.ObjectId.isValid(userId)) return null;
+
+      const user = await UserBasicInfo.findById(userId);
+
+      if (!user) return null;
+
+      if (userId === comment.userId.toString()) {
+        const updateComment = await UserComment.findByIdAndUpdate(id, {
+          comment: data,
+        });
+
+        if (!updateComment) return null;
+
+        const updatedData = await UserComment.findById(id);
+        return updatedData;
+      } else {
+        return null;
+      }
+    } catch (err) {
+      throw err;
+    }
+  };
+
+  //Delete the comment
+  static deleteComment = async (id, userId) => {
+    try {
+      if (!mongoose.Types.ObjectId.isValid(id)) return null;
+
+      if (!mongoose.Types.ObjectId.isValid(userId)) return null;
+
+      const user = await UserBasicInfo.findById(userId);
+
+      if (!user) return null;
+
+      if (userId === comment.userId.toString()) {
+        const deletedComment = await UserComment.findByIdAndDelete(id);
+
+        if (!deletedComment) return null;
+
+        return deletedComment;
+      } else {
+        return null;
+      }
     } catch (err) {
       throw err;
     }
